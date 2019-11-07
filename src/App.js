791fa2c9b4a95f7a16eb948e69f32975ba8a835d
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-06 16:05:26 -0800

    removeItem and buyItem functions dispatch corresponding Action Creators

--- src/App.js
+++ src/App.js
@@ -8,28 +8,30 @@ import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
 //Actions
-import {  }
-
+import { addFeature, removeFeature } from "./actions/actions";
 
 const App = props => {
-  console.log('App props:', props);
+  console.log("App props:", props);
 
-  const removeFeature = item => {
+  const removeItem = item => {
     // dispatch an action here to remove an item
+    props.removeFeature(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.addFeature(item);
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures car={props.car} removeItem={removeItem}/>
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} 
+        buyItem={buyItem}/>
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -37,13 +39,16 @@ const App = props => {
 };
 
 const mapStateToProps = state => {
-return{
-  car: state.car,
-  additionalPrice: state.additionalPrice
-}
+  return {
+    car: state.car,
+    additionalPrice: state.additionalPrice
+  };
 };
 
 export default connect(
   mapStateToProps,
-  {}
+  {
+    addFeature: addFeature,
+    removeFeature: removeFeature
+  }
 )(App);
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,6 +4,7 @@ export const REMOVE_FEATURE = "REMOVE_FEATURE";
 
 //Action Creators
 export const addFeature = item => {
+    console.log('addFeature AC', item);
     return {
         type: ADD_FEATURE, 
         payload: item
@@ -11,6 +12,7 @@ export const addFeature = item => {
 };
 
 export const removeFeature = item => {
+    console.log('removeFeature AC', item);
     return{
         type: REMOVE_FEATURE,
         payload: item
