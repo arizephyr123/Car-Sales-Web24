cdabd3cea00e2840d0b1fa9809087189f09b779e
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-07 15:13:33 -0800

    MVP with more code cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
 
 //Components
 import Header from "./components/Header";
@@ -7,16 +6,9 @@ import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
-//Actions
-import { addFeature, removeFeature } from "./actions/actions";
 
 const App = props => {
   //console.log("App props:", props);
-
-  const removeItem = item => {
-    // dispatch an action here to remove an item
-    props.removeFeature(item);
-  };
 
   return (
     <div className="boxes">
